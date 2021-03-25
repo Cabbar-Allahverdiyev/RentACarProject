9a5027e4c8f0c884eafe40b24a8498d09243cd11
cabbar.allahverdiyev@gmail.com <cabbar.allahverdiyev@gmail.com>
2021-03-25 17:17:24 +0400

    .

---
+++ src/app/models/car.ts
@@ -0,0 +1,10 @@
+export interface Car{
+    carId:number;
+    brandId:number;
+    colorId:number;
+    modelId:number;
+    modelYear:number;
+    carDescription:string;
+    dailyPrice:number;
+
+}
